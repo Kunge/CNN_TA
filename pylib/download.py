17f082981e78b4a74ff9a0e6204bb4979bcded37
Kunge <pengkun@yahoo.com>
2018-07-05 11:12:34 +0800

    finish the stock data download

--- pylib/download.py
+++ pylib/download.py
@@ -4,30 +4,25 @@ import argparse
 import datatype
 
 class StockSpider(object):
-    def __init__(self, code, start="2008-01-01", end="2018-01-01"):
-        self.code = code
-        self.start = start
-        self.end = end
+    def __init__(self, code, start="2000-01-01", end="2018-07-01"):
+        self._code = code
+        self._start = start
+        self._end = end
 
-    def crawl(self):
-        stock_frame = ts.get_k_data(code=self.code, start=self.start, end=self.end, retry_count=30)
-        for index in stock_frame.index:
-            stock_series = stock_frame.loc[index]
-            stock_dict = stock_series.to_dict()
-            stock = datatype.Stock(stock_dict)
-            stock.save_to_file()
-        logging.warning("Finish crawling code: {}, items count: {}".format(self.code, stock_frame.shape[0]))
-
+    def crawl(self, code):
+        stock_his_data = ts.get_k_data( code = code, start=self._start, end = self._end)
+        stock_his_data.to_json('./'+self._code+'.json',orient='records')
 
 def main(args):
-    codes = args.codes
-    # codes = ['sh']
-    for _code in codes:
-        StockSpider(_code, args.start, args.end).crawl()
+    
+    all_basics = ts.get_stock_basics()
+    for code in all_basics.index:
+        StockSpider( code ).crawl( code )
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('codes')
-    args = parser.parse_args()
-    #main(args)
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('codes')
+    #args = parser.parse_args()
+    args = 'maybe used later'
+    main(args)
