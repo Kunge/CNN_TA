917ac4f8d0f536d687c00e8f5fe321323d931abf
Kunge <pengkun@yahoo.com>
2018-07-05 17:59:59 +0800

    use keras engine to train model

--- pylib/data.py
+++ pylib/data.py
@@ -2,19 +2,20 @@
 import numpy as np
 import json
 import os,sys
+import time
 
 
 class HSFeeder:
     def __init__(self,params):
-        self._data_dir = params.data_dir
-        self._batch_size = params.batch_size
-        self._win_len = params.win_len#training window size
-        self._predict_len = params.predict_len#prediction window size
+        self._data_dir = params['data_dir']
+        self._batch_size = params['batch_size']
+        self._win_len = params['win_len']#training window size
+        self._predict_len = params['predict_len']#prediction window size
 
         self._filename_list = self._get_filename_list(self._data_dir)
-        sampel_size = int(len(self._filename_list)*0.6)
+        sample_size = 10#int(len(self._filename_list)*0.6)
         self._training_files = np.random.choice( self._filename_list, size = sample_size )
-        self._dataset = self._load_data()
+        #self._dataset = self._load_data()
     
     def _get_filename_list(self, data_dir):
         filename_list = list()
@@ -23,25 +24,29 @@ class HSFeeder:
                 filename_list.append( os.path.join(root, name) )
         return filename_list
 
-    def _load_data(self, filename_list = self._filename_list):
+    def _load_data(self):
         dataset = dict()
-        for filename in filename_list:
+        for filename in self._filename_list:
             data = json.load( open(filename) )
             dataset[filename] = data
+            time.sleep(0.01)
+        return dataset
 
     def generate_batch(self):
         while True:
             batch_data = self._get_batch_data()
-            yield batch_data
+            yield batch_data['x'], batch_data['y']
 
     def _get_batch_data(self):
         batch_data = dict()
         training = []
         target = []
-        for i in range(self._batch_size):
+        length = 0
+        while length < self._win_len+self._predict_len+10:
             filename = np.random.choice( self._training_files )
-            data = self._dataset[filename]
+            data = json.load( open(filename) )
             length = len(data)
+        for i in range(self._batch_size):
             start = np.random.choice( length-self._win_len-self._predict_len-1 )
             training_data = data[start:start+self._win_len]
             target_data = data[start+self._win_len:start+self._win_len+self._predict_len]
@@ -52,13 +57,22 @@ class HSFeeder:
                 h = k['high']
                 l = k['low']
                 a = np.array([o,c,h,l]).reshape([-1,1])
-                x.append( a )
+                x.append(a)
+            x = np.squeeze(x)
             y=[]
             for k in target_data:
                 c = k['close']
                 y.append(c)
             training.append(x)
             target.append(y)
-        batch_data['x'] = training
-        batch_data['y'] = target
+        batch_data['x'] = np.array(training)
+        batch_data['y'] = np.array(target)
         return batch_data
+
+if __name__ == '__main__':
+    params = {'data_dir':'../data/day','batch_size':10, 'win_len':100, 'predict_len':30}
+    feeder = HSFeeder(params)
+    batch_data = feeder._get_batch_data()
+    print(batch_data['x'])
+    print(batch_data['y'])
+    print(batch_data['x'].shape)
---
+++ pylib/keras_train.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import keras.backend.tensorflow_backend as K
+
+from keras.models import Sequential  
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.pooling import MaxPooling1D
+from keras.optimizers import SGD
+from keras import losses
+import data
+import numpy as np
+import json
+
+model = Sequential()
+model.add(Conv1D(64, 5, padding='valid', input_shape=(120,4)))
+model.add(Activation('relu'))
+model.add(Conv1D(64, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling1D(pool_size=3, strides=2, padding='valid'))
+model.add(Conv1D(128, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(Conv1D(128, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(Conv1D(64, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(Conv1D(64, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(Conv1D(32, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(Conv1D(32, 5, padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling1D(pool_size=3, strides=2, padding='valid'))
+model.add(Flatten())
+model.add(Dense(30))
+
+sgd = SGD(lr=0.000001, decay=0.0005, momentum=0.9, nesterov=True)
+model.compile(loss=losses.mean_squared_error, optimizer=sgd)
+
+params = {'data_dir':'../data/day','batch_size':256, 'win_len':120, 'predict_len':30}
+feeder = data.HSFeeder(params)
+train_generator = feeder.generate_batch()
+while True:
+    model.fit_generator(train_generator, steps_per_epoch = 100, epochs=1, max_q_size=100, workers=5)
+
+js_model = model.to_json()
+model_json = open('model.json','w')
+json.dump(js_model, model_json)
+
+print("test set")
+scores = model.evaluate(X_train,Y_train)
+print(scores)
