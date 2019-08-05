0466bcb36290ecc14b2e53a4ba77587a57848ad7
Kun Peng <kupeng@microsoft.com>
2019-08-05 15:39:07 +0800

    parallel downloading

--- pylib/download.py
+++ pylib/download.py
@@ -2,22 +2,24 @@ import tushare as ts
 import logging
 import argparse
 import datatype
+import multiprocessing
 
-class StockSpider(object):
-    def __init__(self, code, start="2000-01-01", end="2018-07-01"):
-        self._code = code
-        self._start = start
-        self._end = end
+start="1990-01-01"
+end="2019-07-10"
 
-    def crawl(self, code):
-        stock_his_data = ts.get_k_data( code = code, start=self._start, end = self._end)
-        stock_his_data.to_json('./'+self._code+'.json',orient='records')
+def crawl(*args):
+    for code in args:
+        stock_his_data = ts.get_k_data( code = code, start=start, end = end)
+        stock_his_data.to_json('./'+code+'.json',orient='records')
 
 def main(args):
     
-    all_basics = ts.get_stock_basics()
-    for code in all_basics.index:
-        StockSpider( code ).crawl( code )
+    all_basics = ts.get_stock_basics().index
+    all_num = len(all_basics)
+    seg_len = 200
+    a = [tuple(all_basics[x:x+seg_len]) for x in range(0,all_num,seg_len)]
+    with multiprocessing.Pool(processes=10) as pool:
+        pool.starmap(crawl,a)
 
 
 if __name__ == '__main__':
--- pylib/keras_train.py
+++ pylib/keras_train.py
@@ -1,22 +1,23 @@
 import tensorflow as tf
-import keras.backend.tensorflow_backend as K
+import tensorflow.keras.backend as K
 
-from keras.models import Sequential  
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Conv1D
-from keras.layers.pooling import MaxPooling1D
-from keras.optimizers import SGD
-from keras.layers.normalization import BatchNormalization
-from keras import losses
-from keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import Sequential  
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv1D
+from tensorflow.keras.layers import MaxPooling1D
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras import losses
+from tensorflow.keras.callbacks import ModelCheckpoint
 import data
 import numpy as np
 import json
 import sys, os
 
 #model define
+K.set_learning_phase(1)
 model = Sequential()
-model.add(BatchNormalization(axis=1,input_shape=(100,4)))
+model.add(BatchNormalization(axis=1,input_shape=(120,4)))
 model.add(MaxPooling1D(pool_size=5, strides = 2))
 model.add(Conv1D(64, 5, padding='valid'))
 model.add(Activation('relu'))
@@ -78,14 +79,14 @@ model_path = 'mymodel.h5'
 if os.path.exists(model_path):
     model.load_weights(model_path)
 
-sgd = SGD(lr=0.000001, decay=0.0005, momentum=0.9, nesterov=True)
+sgd = SGD(lr=0.00001, decay=0.0005, momentum=0.9, nesterov=True)
 
 model.compile(loss=losses.categorical_crossentropy, optimizer=sgd)
 
 checkpoint = ModelCheckpoint(filepath=model_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-params = {'data_dir':'../data/day','batch_size':128, 'win_len':100, 'predict_len':200}
+params = {'data_dir':'../data/','batch_size':128, 'win_len':120, 'predict_len':200}
 feeder = data.HSFeeder(params)
 train_generator = feeder.generate_batch()
-model.fit_generator(train_generator, steps_per_epoch = 1, epochs=100000, max_q_size=100, workers=5, callbacks=callbacks_list)
+model.fit_generator(train_generator, steps_per_epoch = 1, epochs=100000, workers=1, callbacks=callbacks_list)
